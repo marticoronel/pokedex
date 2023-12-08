f55fc96c37db7bc892f9552571f9718f3e246a52
marticoronel <martinacoronelpiatti@gmail.com>
2023-12-07 21:46:05 -0300

    Inicio de proyecto - Components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Hola Mundo Pokedesk</p>
+    </>
+  )
+}
+
+export default App
+
