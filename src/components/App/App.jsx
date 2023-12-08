0f71b7481f1118fb301671cb82ac4b0cdab8830f
marticoronel <martinacoronelpiatti@gmail.com>
2023-12-07 22:06:59 -0300

    prueba github

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hola Mundo Pokedesk</p>
+      <p>Hola Mundo Pokedesk Prueba Github</p>
     </>
   )
 }
